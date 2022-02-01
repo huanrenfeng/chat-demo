a6144f5300dee438d882a48f44d14946fb9b4360
huanrenfeng <87102139+huanrenfeng@users.noreply.github.com>
2022-02-01 15:23:32 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
+import Vue from 'vue';
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -18,7 +17,7 @@ window.Vue = require('vue');
  */
 
 const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 const app = new Vue({
   el: '#app'
